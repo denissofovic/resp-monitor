c982b43d2d958f06a8ddfd1f8321f58aaa05671a
sofovicdenis <denis.sofovic@fet.ba>
2026-02-25 19:59:47 +0100

    Finished

--- app/src/main/java/com/example/respmonitor/gui/MainScreen.kt
+++ app/src/main/java/com/example/respmonitor/gui/MainScreen.kt
@@ -1,13 +1,11 @@
 package com.example.respmonitor.gui
 
-import CsvLogger
 import GyroSample
 import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
